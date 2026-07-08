c0d0f85358d50f25826ea1fa0593765a42467289
abhishekh002 <abhisekh31tiwari@gmail.com>
2026-07-08 22:16:35 +0530

    commited

--- src/main/java/ReturnWordCount.java
+++ src/main/java/ReturnWordCount.java
@@ -1,10 +1,13 @@
-
 public class ReturnWordCount {
     /**
      * @param in A String representing a sentence, with words delineated by spaces.
      * @return return the amount of words in a string.
      */
-    public int count(String in){
-        return 0;
+    public int count(String in) {
+        if (in == null || in.trim().isEmpty()) {
+            return 0;
+        }
+
+        return in.trim().split("\\s+").length;
     }
 }
